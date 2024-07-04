6033aa7394e8cded19213dc2bba8ca0f642b698e
Rajan Maurya <rajanmaurya154@gmail.com>
2024-07-03 21:12:16 -0400

    Fix: update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="pdf_analyzer",
-    version="1.0",
-    description="PDF Analyzer to extract tables from PDFs and save as CSV",
-    author="Your Name",
-    packages=["."],
+    name='BruckerCo',
+    version='1.0',
+    packages=find_packages(),
     install_requires=[
-        "pdfplumber",
-        "pandas",
-        "tk"
+        'pdfplumber~=0.11.1',
+        'pandas~=2.2.2',
+        'tk',
+        'PyQt5~=5.15.10',
+        'setuptools~=68.2.0',
+        'camelot-py[cv]',
+        'PyQt5',
+        'PyPDF2~=3.0.1',
+        'opencv-python',
+        'PyMuPDF',
+        'pytesseract~=0.3.10',
+        'openai~=0.27.0',
+        'Pillow==10.0.1',
+        'opencv-python-headless==4.10.0.82',
+        'fitz~=0.0.1.dev2',
+        'pyinstaller'
     ],
     entry_points={
-        "console_scripts": [
-            "pdf_analyzer = main:main"
-        ]
-    }
+        'console_scripts': [
+            'bruckercopy = BruckerCo:main',
+        ],
+    },
 )
